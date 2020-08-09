6ba28c457e21d55e1ba1940980960dbbd525ac02
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-08-09 12:56:30 +0200

    add /logout and /google route in router file

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -3,3 +3,13 @@ const router = require("express").Router();
 router.get("/login", (req, res) => {
   res.render("login");
 });
+
+router.get("/logout", (req, res) => {
+  // TODO: Handle with passport
+  res.send("Loggin out");
+});
+
+router.get("/google", (req, res) => {
+  // TODO: handle with passport
+  res.send("Loggin in with google");
+});
