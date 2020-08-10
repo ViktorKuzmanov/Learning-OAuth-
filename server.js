7e28d2f7873aee942c2a1d678950d6fd771fa9f1
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-08-11 00:16:58 +0200

    add cookie session on server

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const authRoutes = require("./routes/auth-routes");
 const passportSetup = require("./config/passport-setup");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 
 const app = express();
 
@@ -14,6 +16,16 @@ mongoose.connect(keys.mongoDB.dbURI, {
 });
 
 app.use("/auth", authRoutes);
+app.use(
+  cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [keys.session.cookieKey],
+  })
+);
+
+// Initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", (req, res) => {
   res.render("home");
