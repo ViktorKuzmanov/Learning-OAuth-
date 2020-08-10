dc584dd3fc1b51992571e351f6d1ee6041674ec0
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-08-11 00:02:33 +0200

    deserialize user - retrieve the user id from cookie

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -8,6 +8,12 @@ passport.serializeUser(function (user, done) {
   done(null, user.id);
 });
 
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
